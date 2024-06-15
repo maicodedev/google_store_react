5658b484a2bbc9d3f01b9d7233a843d4bc0f72f8
pilimuino <pili.muino@gmail.com>
2024-06-15 21:13:24 +0200

    añado cambios en style.css y modifico jxs

--- src/pages/Earbuds.jsx
+++ src/pages/Earbuds.jsx
@@ -11,75 +11,74 @@ const Earbuds = () => {
   return (
     <main>
       <div className="see">
-        <img src={ArrowIcon} alt="Arrow" className="arrow" />
-        <p className="seepro">See product details</p>
+                  <img src={ArrowIcon} alt="Arrow" className="arrow" />
+                  <p className="seepro">See product details</p>
       </div>
       <section className="container">
-        <div className="column1">
-          <div className="ears_container">
-            <div className="ear">
-              <img src={Earbuds01} alt="earbuds" className="cimage" />
+                <div className="column1">
+                        <div className="ears_container">
+                                    <div className="ear">
+                                            <img src={Earbuds01} alt="earbuds" className="cimage" />
+                                    </div>
+                                    <div className="ear">
+                                              <img src={Earbuds02} alt="earbuds" className="cimage" />
+                                    </div>
+                                    <div className="ear">
+                                              <img src={Earbuds03} alt="earbuds" className="cimage" />
+                                    </div>
+                                    <div className="ear">
+                                            <img src={Earbuds04} alt="earbuds" className="cimage" />
+                                    </div>
+                        </div>
+                </div>
+                    <div className="column2"> 
+                            <div className="rectangle">
+                                    <img src={Earbuds01} alt="earbuds" />
+                            </div>
+                    </div> 
+        <div className="column3">
+                      <div className="product">
+                                  <h1 className="title">Google Pixel Buds Pro</h1>
+                              
+                                  <p>Music & Sound</p>
+                                  
+                                  <h3>229 €</h3>
+                      </div>
+                      
+                      <div>
+                        <div className="line"></div>
+                        <p>Choose a color</p>
+                                      <div className="container1">
+                                                <div className="circle blue"></div>
+                                                <div className="circle beige"></div>
+                                                <div className="circle grey"></div>
+                                                <div className="circle lila"></div>
+                                                <div className="circle green"></div>
+                                                <div className="circle red"></div>
+                                      </div>
+                      </div>
+                <div className="addtocartcontainer">
+                          <div>
+                            <h2 className="price">229 €</h2>
+                            <div className="line1"/>
+                          </div>
+                <div className="addtocartline">
+                          <button className="numberbox">
+                                    <a href="#" className="number">1</a>
+                                    <a href="#" className="triangle">▾</a>
+                          </button>
+                  
+                          <button className="addtocart">Add to Cart</button>
+                  </div>
+           
+                      <div className="delivery">
+                        <img src={DeliveryIcon} alt="deliveryicon" className="deliveryicon" />
+
+                        <p><b>Delivers 29 April to </b><span style={{ color: '#1A73E8' }}><b>08023</b></span></p>
+                      </div>
             </div>
-            <div className="ear">
-              <img src={Earbuds02} alt="earbuds" className="cimage" />
-            </div>
-            <div className="ear">
-              <img src={Earbuds03} alt="earbuds" className="cimage" />
-            </div>
-            <div className="ear">
-              <img src={Earbuds04} alt="earbuds" className="cimage" />
-            </div>
-          </div>
+          
         </div>
-        <div className="column2">
-          <div className="rectangle">
-            <img src={Earbuds01} alt="earbuds" />
-          </div>
-        </div>
-        <section className="column3">
-          <div>
-            <h1 className="title">Google Pixel Buds Pro</h1>
-            <br />
-            <p>Music & Sound</p>
-            <br />
-            <h3>229 €</h3>
-          </div>
-          <div className="line"></div>
-          <div>
-            <br />
-            <br />
-            <p>Choose a color</p>
-            <div className="container1">
-              <div className="circle blue"></div>
-              <div className="circle beige"></div>
-              <div className="circle grey"></div>
-              <div className="circle lila"></div>
-              <div className="circle green"></div>
-              <div className="circle red"></div>
-            </div>
-          </div>
-          <div className="addtocartcontainer">
-            <div>
-              <h2 className="price">229 €</h2>
-            </div>
-            <div className="addtocartline"></div>
-            <div className="numberbox">
-              <a href="#" className="number">1</a>
-              <a href="#" className="triangle">▾</a>
-            </div>
-            <div>
-              <button className="addtocart">Add to Cart</button>
-            </div>
-            <div className="delivblock">
-              <div className="delivery">
-                <img src={DeliveryIcon} alt="deliveryicon" className="deliveryicon" />
-              </div>
-              <div className="deliveryinfo">
-                <p><b>Delivers 29 April to </b><span style={{ color: '#1A73E8' }}><b>08023</b></span></p>
-              </div>
-            </div>
-          </div>
-        </section>
       </section>
     </main>
   );
--- src/pages/Watches.jsx
+++ src/pages/Watches.jsx
@@ -9,64 +9,62 @@ import './Watches.css';
 
 const Smartwatch = () => {
   return (
-    <main>
-      <div className="see">
-        <img src={ArrowIcon} alt="Arrow" className="arrow" />
-        <p className="seepro">See product details</p>
-      </div>
-      <section className="container">
-        <div className="column2">
-          <div className="rectangle">
-            <img src={smartwatch_black} alt="smartwatch" />
+      <main>
+          <div className="see">
+                          <img src={ArrowIcon} alt="Arrow" className="arrow" />
+                          <p className="seepro">See product details</p>
           </div>
-        </div>
-        <div className="column3">
-          <div>
-            <h2>Fitbit Inspire 3</h2><br />
-            <p>Health & Fitness Tracker</p><br />
-            <h3>99,95 €</h3>
-          </div>
-          <div className="line"></div>
-          <div>
-            <p>Choose a color</p>
-            <div className="models">
-              <section className="colormodels">
-                <section className="model1">
-                  <img src={smartwatch_black} alt="blacksmartwatch" />
-                  <section className="namemodel1">Midnight Zen</section>
-                </section>
-                <section className="model2">
-                  <img src={smartwatch_pink} alt="pinksmartwatch" />
-                  <section className="namemodel2">Lilac Bliss</section>
-                </section>
-                <section className="model3">
-                  <img src={smartwatch_yellow} alt="yellowsmartwatch" />
-                  <section className="namemodel3">Morning Glow</section>
-                </section>
-              </section>
-            </div>
-          </div>
-          <section className="addtocartcontainer">
-            <div>
-              <h2>99,95 €</h2>
-            </div>
-            <div className="addtocartline"></div>
-            <div className="numberbox">
-              <a href="#" className="number">1</a>
-              <a href="#" className="triangle">▾</a>
-            </div>
-            <div>
-              <button className="addtocart">Add to Cart</button>
-            </div>
-            <div className="delivery">
-              <img src={DeliveryIcon} alt="deliveryicon" className="deliveryicon" />
-            </div>
-            <div className="deliveryinfo">
-              <p><b>Delivers 29 April to </b><span style={{ color: '#1A73E8' }}><b>08023</b></span></p>
-            </div>
+           <section className="container">
+                                  <div className="column2">
+                                                <div className="rectangle">
+                                                          <img src={smartwatch_black} alt="smartwatch" />
+                                                </div>
+                                  </div>
+                                  <div className="column3">
+                                                <div className="product">
+                                                        <h1 className="title">Fitbit Inspire 3</h1><br/>
+                                                        <p>Health & Fitness Tracker</p><br />
+                                                        <h3>99,95 €</h3>
+                                                </div>
+                                                <div className="line"/>
+                                                <div>
+                                                   <p>Choose a color</p>
+                                                <div className="models">
+                                                <section className="colormodels">
+                                                    <section className="model1">
+                                                              <img src={smartwatch_black} alt="blacksmartwatch" />
+                                                              <section className="namemodel1">Midnight Zen</section>
+                                                    </section>
+                                                    <section className="model2">
+                                                              <img src={smartwatch_pink} alt="pinksmartwatch" />
+                                                              <section className="namemodel2">Lilac Bliss</section>
+                                                    </section>
+                                                    <section className="model3">
+                                                            <img src={smartwatch_yellow} alt="yellowsmartwatch" />
+                                                            <section className="namemodel3">Morning Glow</section>
+                                                    </section>
+                                               </section>
+                                           </div>
+                                       </div>
+                                        <div className="addtocartcontainer">
+                                                  <div>
+                                                      <h2>99,95 €</h2>
+                                                      <div className="line1"/>
+                                                  </div>
+                                            <div className="addtocartline">
+                                                            <button className="numberbox">
+                                                                        <a href="#" className="number">1</a>
+                                                                        <a href="#" className="triangle">▾</a>
+                                                            </button>
+                                                            <button className="addtocart">Add to Cart</button>
+                                                      </div>
+                                                  <div className="delivery">
+                                                             <img src={DeliveryIcon} alt="deliveryicon" className="deliveryicon" />
+                                                             <p><b>Delivers 29 April to </b><span style={{ color: '#1A73E8' }}><b>08023</b></span></p>
+                                                  </div>
+                                        </div>
+                                </div>
           </section>
-        </div>
-      </section>
     </main>
   );
 };
